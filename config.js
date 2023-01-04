6e9d35a15d0b1f7e0f280380eeb7bf0ff93c916e
rhumblinemaps <121906107+rhumblinemaps@users.noreply.github.com>
2023-01-04 16:30:31 -0500

    Update config.js

--- config.js
+++ config.js
@@ -10,23 +10,23 @@ var config = {
     theme: 'dark',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'The Title Text of this Story',
-    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
-    byline: 'By a Digital Storyteller',
+    title: 'Birds of the Texas Islands',
+    subtitle: 'Audubon Texas protects bird habitat on nearly 200 islands in the Gulf of Mexico',
+    byline: 'By Rhumbline Maps',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
             id: 'slug-style-id',
             alignment: 'left',
             hidden: false,
-            title: 'Display Title',
+            title: 'The Texas Coast',
             image: './path/to/image/source.png',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
-                center: [-122.418398, 37.759483],
-                zoom: 8.5,
-                pitch: 60,
-                bearing: 0
+                center: [-96.41804, 28.40959],
+                zoom: 7.50,
+                pitch: 51.36,
+                bearing: -43.20
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
