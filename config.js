a6e732ca97337113ba108bb31da345dab690ca38
rhumblinemaps <121906107+rhumblinemaps@users.noreply.github.com>
2023-02-01 15:32:55 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 var config = {
-    style: 'mapbox://styles/mapbox/satellite-v9',
+    style: 'mapbox://styles/vincentrhumbline/clci7ewl7003c14nbmkls5zsn',
     accessToken: 'pk.eyJ1IjoidmluY2VudHJodW1ibGluZSIsImEiOiJjbGNpNHZoam4wOGk5M29wZXFsaGdvdWViIn0.XvKO9DMeISCMCch_tNqUsg',
     showMarkers: false,
     markerColor: '#3FB1CE',
     //projection: 'equirectangular',
     //Read more about available projections here
     //https://docs.mapbox.com/mapbox-gl-js/example/projections/
-    inset: true,
+    inset: false,
     theme: 'dark',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
     title: 'Birds of the Texas Islands',
-    subtitle: 'Audubon Texas protects bird habitat on nearly 200 islands in the Gulf of Mexico',
-    byline: 'By Rhumbline Maps',
+    subtitle: 'The coast of Texas is lined with bird colony islands, and traced with annual migration pathways. But what happens when the sea rises?',
+    byline: 'Audubon Texas',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
@@ -20,8 +20,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'The Texas Coast',
-            image: './path/to/image/source.png',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            image: './assets/images/tyler-moulton-LOclOrfq6XI-unsplash.jpg',
+            description: 'The coast of Texas is lined with bird colony islands, and traced with annual migration pathways. But what happens when the sea rises?',
             location: {
                 center: [-96.41804, 28.40959],
                 zoom: 7.50,
@@ -78,10 +78,10 @@ var config = {
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-97.37563, 26.79609],
-                zoom: 10.33,
-                pitch: 57.56,
-                bearing: -28.00
+                center: { lon: -97.31536, lat: 26.39031 },
+                zoom: 13.88,
+                pitch: 49.00,
+                bearing: -40.00
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
